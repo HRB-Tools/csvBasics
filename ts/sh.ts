75793d526b457393473d6411689f1b20b52860ca
UKayeF <nilseidmann@gmail.com>
2019-02-20 12:50:24 +0100

    jetzte

--- ts/sh.ts
+++ ts/sh.ts
@@ -2,10 +2,8 @@ export { sollHaben }
 
 function sollHaben(arr, indexSH, indexUmsatz){
     for ( let i = 0; i < arr.length; i++ ){
-        for ( let j = 0; j < arr[i].length; j++ ){
-            if ( arr[i][indexSH] = 'S' ){
-                arr[i][indexUmsatz] = '-' + arr[i][indexUmsatz]
-            }
+        if ( arr[i][indexSH] = 'S' ){
+            arr[i][indexUmsatz] = '-' + arr[i][indexUmsatz]
         }
     }
     return arr

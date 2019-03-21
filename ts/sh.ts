c33d8eb82697da2980a190a65586545c5527eaba
nilseidmann <nilseidmann@gmail.com>
2019-03-21 09:40:46 -0400

    string quotes

--- ts/sh.ts
+++ ts/sh.ts
@@ -2,7 +2,7 @@ export { sollHaben }
 
 function sollHaben(arr, indexSH, indexUmsatz){
     for ( let i = 2; i < arr.length; i++ ){
-        if ( arr[i][indexSH] == 'S' ){
+        if ( arr[i][indexSH] == '"S"' ){
             arr[i][indexUmsatz] = '-' + arr[i][indexUmsatz]
         }
     }

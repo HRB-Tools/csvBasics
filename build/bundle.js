5a2f23db16ce2e189e0638ddb548f4756c9284f2
nilseidmann <nilseidmann@gmail.com>
2019-03-21 09:32:28 -0400

    = -> ==

--- build/bundle.js
+++ build/bundle.js
@@ -73,7 +73,7 @@
 
     function sollHaben(arr, indexSH, indexUmsatz) {
         for (let i = 2; i < arr.length; i++) {
-            if (arr[i][indexSH] = 'S') {
+            if (arr[i][indexSH] == 'S') {
                 arr[i][indexUmsatz] = '-' + arr[i][indexUmsatz];
             }
         }
